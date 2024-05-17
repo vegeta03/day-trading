bdb3953757eccaf829f52ed65421a49db0193bcd
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-17 05:40:20 +0530

    boilerplate

---
+++ src/main/java/com/mine/daytrading/DayTradingApplication.java
@@ -0,0 +1,13 @@
+package com.mine.daytrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mine/daytrading/DayTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mine.daytrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
